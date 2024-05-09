1a42248f739937de85889096a1cce37b7822932c
Williams Hernandez <hernandw@gmail.com>
2024-05-08 21:22:25 -0400

    :sparkles: configuracion de rutas y nombre de la imagen

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,19 @@
 import express from "express";
+import Jimp from "jimp";
+import { v4 as uuidv4 } from 'uuid';
+import moment from "moment";
+import path from "path";
+const __dirname = path.resolve();
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get('/crear', async (req, res) => {
+    const id = uuidv4().slice(0, 4);
+    const imagen = await Jimp.read('https://placedog.net/500/500')
+    await imagen
+        .resize(250, Jimp.AUTO)
+        .sepia()
+        .writeAsync(`uploads/imagen-${moment().format('X')}.png`)
+    res.send('imagen creada con exito!');
 })
 
 export default router
